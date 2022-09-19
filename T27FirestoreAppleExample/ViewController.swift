8348a0e08f66d01faeb32ea6350ca5887113dbd7
Muhammet Ali Amanvermez <maliamanvermez@gmail.com>
2022-09-19 14:35:51 +0300

    Initial Commit

---
+++ T27FirestoreAppleExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T27FirestoreAppleExample
+//
+//  Created by NeonApps on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
